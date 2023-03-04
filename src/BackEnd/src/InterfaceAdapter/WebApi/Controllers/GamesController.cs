7b1f8727690d63c784159798bd7e5f49e227a8a6
Rick Su <35659961+ricksu978@users.noreply.github.com>
2023-03-04 16:47:51 +1100

    Question from 葡萄

---
+++ src/BackEnd/src/InterfaceAdapter/WebApi/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Wsa.Gaas.Werewolf.Application.Common;
+using Wsa.Gaas.Werewolf.Application.UseCases;
+using Wsa.Gaas.Werewolf.Domain.Common;
+using Wsa.Gaas.Werewolf.Domain.Events;
+using Wsa.Gaas.Werewolf.WebApi.Endpoints;
+
+namespace Wsa.Gaas.Werewolf.WebApi.Controllers
+{
+    public class GamesController : Controller, IPresenter<GameCreatedEvent>
+    {
+        [HttpPost("/games")]
+        [AllowAnonymous]
+        public IActionResult Create(
+            CreateGameRequest request,
+            [FromServices] UseCase<CreateGameRequest, GameCreatedEvent> useCase
+        )
+        {
+            useCase.ExecuteAsync(request, this, default);
+
+            return Ok();
+        }
+
+        public Task PresentAsync(GameCreatedEvent gameEvent, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
