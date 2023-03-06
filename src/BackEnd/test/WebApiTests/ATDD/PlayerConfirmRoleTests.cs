f4513ce68a71c8f6e8cb6830c8111d800d9b8641
chimmyJay <chimm9jay@gmail.com>
2023-03-07 01:20:06 +0800

    feat : (ATDD) role_count when 10,11,12 players

--- src/BackEnd/test/WebApiTests/ATDD/PlayerConfirmRoleTests.cs
+++ src/BackEnd/test/WebApiTests/ATDD/PlayerConfirmRoleTests.cs
@@ -16,34 +16,20 @@ namespace Wsa.Gaas.Werewolf.WebApiTests.ATDD;
 public class PlayerConfirmRoleTests : TestsBase
 {
     [Test]
-    public async Task PlayerConfirmRole()
+    public async Task player_confirm_role()
     {
         //Init
         var game = GivenGame(gameStatus: GameStatus.Created);
-        var manualDelay = new ManualResetEventSlim();
-
-        var playerIds = Enumerable.Range(1, 9)
-                                  .Select(o => (long) o)
-                                  .ToImmutableList();
-
-        FakeTaskService.Delay(Arg.Any<TimeSpan>())
-                       .Returns(_ =>
-                                {
-                                    manualDelay.Wait();
-
-                                    return Task.CompletedTask;
-                                });
+        var manualStopAction = GivenConfirmRoleDurationByManual();
 
         //Before started
         var (responseBeforeStarted, _) = await ExecutePlayerConfirmRole(game);
         responseBeforeStarted!.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
 
         //started
-        await ExecuteStartGame(game.RoomId, playerIds);
+        await ExecuteStartGame(game.RoomId, GetPlayerIds(9));
         var (responseAfterStarted, _) = await ExecutePlayerConfirmRole(game);
-
         responseAfterStarted!.IsSuccessStatusCode.Should().BeTrue();
-
         await LetBulletFly();
 
         await FakeTaskService.Received(1)
@@ -51,9 +37,8 @@ public class PlayerConfirmRoleTests : TestsBase
 
         //Time up
         HubListenOn(nameof(PlayerRoleConfirmationStoppedEvent));
-        manualDelay.Set();
+        manualStopAction.Invoke();
         await LetBulletFly();
-
         GetGame(game)!.Status.Should().Be(GameStatus.PlayerRoleConfirmationStopped);
 
         FakeAction.Received(1)
@@ -64,17 +49,61 @@ public class PlayerConfirmRoleTests : TestsBase
         responseAfterStopped!.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
     }
 
-    [Test]
-    public async Task role_count()
+    [TestCase(9)]
+    [TestCase(10)]
+    [TestCase(11)]
+    [TestCase(12)]
+    public async Task role_count(int playerCount)
     {
         //Init
-
-        var playerIds = Enumerable.Range(1, 9)
-                                  .Select(o => (long) o)
-                                  .ToImmutableList();
-
+        _ = GivenConfirmRoleDurationByManual();
+        var playerIds = GetPlayerIds(playerCount);
         var game = GivenStartedGame(playerIds);
+        var roleCountDict = GetRoleCountDict(playerCount);
 
+        //Assert
+        foreach (var playerId in playerIds)
+        {
+            var (_, result) = await ExecutePlayerConfirmRole(game, playerId);
+            roleCountDict[result!.Role] -= 1;
+        }
+
+        foreach (var (role, count) in roleCountDict)
+            count.Should().Be(0, $"{role} has wrong count");
+    }
+
+    private static ImmutableList<long> GetPlayerIds(int playerCount)
+    {
+        return Enumerable.Range(1, playerCount)
+                         .Select(o => (long) o)
+                         .ToImmutableList();
+    }
+
+    private static Dictionary<string, int> GetRoleCountDict(int playerCount)
+    {
+        var roleCountDict = new Dictionary<string, int>
+                            {
+                                { nameof(Domain.Entities.Rules.Werewolf), 3 },
+                                { nameof(Villager), 3 },
+                                { nameof(Hunter), 1 },
+                                { nameof(Witch), 1 },
+                                { nameof(Seer), 1 }
+                            };
+
+        if (playerCount >= 10)
+            roleCountDict[nameof(Villager)] += 1;
+
+        if (playerCount >= 11)
+            roleCountDict.Add(nameof(AlphaWerewolf), 1);
+
+        if (playerCount >= 12)
+            roleCountDict.Add(nameof(Guardian), 1);
+
+        return roleCountDict;
+    }
+
+    private Action GivenConfirmRoleDurationByManual()
+    {
         var manualDelay = new ManualResetEventSlim();
 
         FakeTaskService.Delay(Arg.Any<TimeSpan>())
@@ -85,29 +114,9 @@ public class PlayerConfirmRoleTests : TestsBase
                                     return Task.CompletedTask;
                                 });
 
-
-        await ExecuteStartGame(game.RoomId, playerIds);
-
-        //TODO all role need appear
-
-        var roleCountDict = new Dictionary<string, int>();
-        roleCountDict.TryAdd(nameof(Domain.Entities.Rules.Werewolf), 3);
-        roleCountDict.TryAdd(nameof(Villager), 3);
-        roleCountDict.TryAdd(nameof(Hunter), 1);
-        roleCountDict.TryAdd(nameof(Witch), 1);
-        roleCountDict.TryAdd(nameof(Seer), 1);
-
-        foreach (var playerId in playerIds)
-        {
-            var (_, result) = await ExecutePlayerConfirmRole(game, playerId);
-
-            roleCountDict[result!.Role] -= 1;
-        }
-
-
-        foreach (var (role, count) in roleCountDict)
-            count.Should().Be(0, $"{role} has wrong count");
+        return manualDelay.Set;
     }
+
 
     private static async Task LetBulletFly()
     {
