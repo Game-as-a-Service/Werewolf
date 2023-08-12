88861696e05838b3ae175d90fa19f542fb71d6a4
Rick Su <35659961+ricksu978@users.noreply.github.com>
2023-08-13 00:57:23 +1000

    Done 宣布昨晚結果

--- src/BackEnd/src/Domain/Events/PlayerDiedGameEvent.cs
+++ src/BackEnd/src/Domain/Events/PlayerDiedGameEvent.cs
@@ -2,8 +2,17 @@
 
 namespace Wsa.Gaas.Werewolf.Domain.Events;
 
+public enum SkillTrigger
+{
+    None,
+    Shot,
+}
+
 public class PlayerDiedGameEvent : GameEvent
 {
+    // 讓前端知道，技能觸發
+    public SkillTrigger Skill { get; set; }
+    
     public PlayerDiedGameEvent(Game data) : base(data)
     {
     }
